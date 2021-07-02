4f139013c0aee1f5987d602be743cf0a52c6efad
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-02 12:40:24 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	// you can output 0, 0, n because the number given is always a Fibonacci number
+	cout << "0 0 " << n << '\n';
+	return 0;
+}
